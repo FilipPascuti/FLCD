108b7a87bb0c1442138fd34cdbb948742cc73afe
Filip Pascuti <filip@192-168-0-100.rdsnet.ro>
2021-11-29 15:18:34 +0200

    added parsing algorithm

---
+++ lab parser/main.py
@@ -0,0 +1,17 @@
+from grammar import Grammar
+from recursive_descendant import Recursive_descendent
+
+def main():
+    grammar = Grammar()
+    grammar.read_grammar("g1.txt")
+
+    rd = Recursive_descendent(grammar, "aacbc")
+
+    productions, result = rd.start()
+
+    print(result)
+    if result == "success":
+        print(productions)
+
+if __name__ == "__main__":
+    main()
